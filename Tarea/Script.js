a46f0d8e16b01978ae82f309b76e2176f00cf16a
Jfs056 <35733673+Jfs056@users.noreply.github.com>
2018-04-05 12:08:48 -0600

    Add files via upload

---
+++ Tarea/Script.js
@@ -0,0 +1,11 @@
+var myweb = angular.module ("myweb",[]);
+myweb.controller("Login", function($scope){
+	//Funcion de Inicio de Sesion
+	$scope.usuario="";
+	$scope.comtra="":
+
+	$scope.inicio = function
+	alert ("ya me puedo graduar")
+$scope.Inicio= function()
+
+});
